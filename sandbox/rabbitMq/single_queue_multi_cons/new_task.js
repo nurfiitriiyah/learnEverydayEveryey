0a6f579b0b29c90c9a291df444879c06815af700
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-08-13 13:26:44 +0700

    .....

--- sandbox/rabbitMq/single_queue_multi_cons/new_task.js
+++ sandbox/rabbitMq/single_queue_multi_cons/new_task.js
@@ -1,26 +1,22 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib');
+const CONN_URL = 'amqp://localhost';
 
-amqp.connect('amqp://localhost', function (error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+async function sendThem() {
+    try {
+        var connect = await amqp.connect(CONN_URL)
+        var create = await connect.createChannel()
         var queue = 'task_queue';
         var msg = process.argv.slice(2).join(' ') || "Hello World!";
+        var assertqueue = await create.assertQueue('task_queue', {durable: true})
+        var pushqueue = await create.sendToQueue('task_queue', Buffer.from(msg))
+        setTimeout(function () {
+            create.close();
+            process.exit(0);
+        }, 500);
 
-        channel.assertQueue(queue, {
-            durable: true
-        });
-        channel.sendToQueue(queue, Buffer.from(msg), {
-            persistent: true
-        });
-        console.log(" [x] Sent '%s'", msg);
-    });
-    setTimeout(function () {
-        connection.close();
-        process.exit(0);
-    }, 500);
-});
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+sendThem()
--- sandbox/rabbitMq/single_queue_multi_cons/worker.js
+++ sandbox/rabbitMq/single_queue_multi_cons/worker.js
@@ -1,31 +1,53 @@
-var amqp = require('amqplib/callback_api');
+// var amqp = require('amqplib/callback_api');
+//
+// amqp.connect('amqp://localhost', function (error0, connection) {
+//     if (error0) {
+//         throw error0;
+//     }
+//     connection.createChannel(function (error1, channel) {
+//         if (error1) {
+//             throw error1;
+//         }
+//         var queue = 'task_queue';
+//         // This makes sure the queue is declared before attempting to consume from it
+//         channel.assertQueue(queue, {
+//             durable: true
+//         });
+//         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+//         channel.consume(queue, function(msg) {
+//             var secs = msg.content.toString().split('.').length - 1;
+//
+//             console.log(" [x] Received %s", msg.content.toString());
+//             setTimeout(function() {
+//                 console.log(" [x] Done");
+//             }, secs * 1000);
+//         }, {
+//             // automatic acknowledgment mode,
+//             // see https://www.rabbitmq.com/confirms.html for details
+//             // noAck: false nanti dia kalo amti maka hasilnya dialihin ke yg idup
+//             noAck: false
+//         });
+//     });
+// });
 
-amqp.connect('amqp://localhost', function (error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+
+var amqp = require('amqplib');
+const CONN_URL = 'amqp://localhost';
+
+async function getThem() {
+    try{
         var queue = 'task_queue';
-        // This makes sure the queue is declared before attempting to consume from it
-        channel.assertQueue(queue, {
-            durable: true
-        });
+        var connect = await amqp.connect(CONN_URL)
+        var create = await connect.createChannel()
+        var assertqueue = await create.assertQueue(queue, {durable: true})
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-        channel.consume(queue, function(msg) {
-            var secs = msg.content.toString().split('.').length - 1;
-
+        var create_queue = await create.consume(queue, msg =>{
             console.log(" [x] Received %s", msg.content.toString());
-            setTimeout(function() {
-                console.log(" [x] Done");
-            }, secs * 1000);
-        }, {
-            // automatic acknowledgment mode,
-            // see https://www.rabbitmq.com/confirms.html for details
-            // noAck: false nanti dia kalo amti maka hasilnya dialihin ke yg idup
-            noAck: false
-        });
-    });
-});
+        }, {noAck: true})
+    }catch (e) {
+        console.log("-------------------")
+        console.log(e.message)
+    }
+}
+
+getThem()
